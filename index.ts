b9a58beb83b44c938ce38ecf55d7801ea7b4b0b4
Gabriel Brun <lydstyl@gmail.com>
2025-04-16 18:25:34 +0200

    Add calculation for rent review.

--- index.ts
+++ index.ts
@@ -16,22 +16,54 @@ const doc = new Docxtemplater(zip, {
   linebreaks: true
 })
 
-// Données à injecter dans les {{ variables }}
+// Données à injecter dans les { variables }
+// BAILLEUR: 'SCI LOGIS ANGE\n259 rue de Wallers\n59590 RAISMES',
+const BAILLEUR = 'Gabriel BRUN\n259 rue de Wallers\n59590 RAISMES'
+
+const CIVILITÉ = 'Monsieur'
+// LOCATAIRE= 'ZUREK\nAppartement n°3\n32 B rue Henri Durre\n59590 RAISMES'
+const LOCATAIRE =
+  'TESTELIN Cyrille\n1 bis cour Patrice\n59230 ST AMAND LES EAUX'
+
+const DATE_COURRIER = '16/04/2025'
+
+const TYPE_INDICE = 'Indice de Référence des Loyers (IRL)'
+
+// TRIMESTRE= '2ᵉ'
+const TRIMESTRE = '1er'
+
+const nind = 145.47
+const NIND = nind.toFixed(2).replace('.', ',')
+const ai = 143.46
+const AI = ai.toFixed(2).replace('.', ',')
+
+const lhc = 457.7
+const LHC = lhc.toFixed(2).replace('.', ',')
+const nlhc = lhc * (nind / ai)
+const NLHC = nlhc.toFixed(2).replace('.', ',')
+const charges = 0
+const CHARGES = charges.toFixed(2).replace('.', ',')
+const NOUVEAU_LOYER = (nlhc + charges).toFixed(2).replace('.', ',')
+const REGLEMENT = 'juin 2025'
+
+// SIGNATURE: 'Gabriel Brun, gérant de la SCI LOGIS ANGE'
+const SIGNATURE = 'Gabriel BRUN'
+
 doc.setData({
-  BAILLEUR: 'SCI LOGIS ANGE\n259 rue de Wallers\n59590 RAISMES',
-  CIVILITÉ: 'Monsieur',
-  LOCATAIRE: 'ZUREK\nAppartement n°3\n32 B rue Henri Durre\n59590 RAISMES',
-  DATE_COURRIER: '25/12/2024',
-  TYPE_INDICE: 'Indice de Référence des Loyers',
-  TRIMESTRE: '2ᵉ',
-  NIND: '145,17',
-  LHC: '492,00',
-  AI: '140,65',
-  NLHC: '507,96',
-  CHARGES: '54',
-  NOUVEAU_LOYER: '561,96',
-  REGLEMENT: 'février 2025',
-  SIGNATURE: 'Gabriel Brun, gérant de la SCI LOGIS ANGE'
+  BAILLEUR,
+  CIVILITÉ,
+  LOCATAIRE,
+  DATE_COURRIER,
+  TYPE_INDICE,
+  TRIMESTRE,
+  NIND,
+  AI,
+  LHC,
+  NLHC,
+  CHARGES,
+  NOUVEAU_LOYER,
+  REGLEMENT,
+  SIGNATURE
 })
 
 try {
